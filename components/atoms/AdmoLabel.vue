deb2758b1605c63fb59e4fa07b3526706b8ce0cd
Dennis Fink <dennis.fink@kernpunkt.de>
2022-06-27 21:00:47 +0200

    Added AdmoLabel and display more contact data

---
+++ components/atoms/AdmoLabel.vue
@@ -0,0 +1,19 @@
+<template>
+  <label :for="forAttr"><span class="font-bold">{{ text }}</span></label>
+</template>
+
+<script>
+export default {
+  props: {
+    forAttr: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- components/molecules/overlays/AdmoOverlay.vue
+++ components/molecules/overlays/AdmoOverlay.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-80 p-16">
-    <AdmoContainer class="h-full flex items-center">
+    <AdmoContainer class="h-full flex items-center lg:max-w-3xl">
       <div class="bg-white p-12 w-full relative">
         <slot></slot>
         <div @click="closeOverlay" class="shadow-md cursor-pointer -top-4 -right-4 bg-white w-8 h-8 absolute top-0 right-0">
